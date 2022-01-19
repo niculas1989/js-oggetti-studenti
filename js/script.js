0e2310fb48200f3f2ffb1ffbaa14baa396344833
niculas1989 <nicolas.maranzano@libero.it>
2022-01-19 14:38:48 +0100

    add options for HTML - user experience

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 console.log('JS OK');
+
+/*
+Descrizione:
+1. Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
+2. Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
+3. Creare un array di oggetti di studenti (ossìa oggetti con le proprietà sopra descritte).
+4. Ciclare su tutti gli studenti e stampare per ognuno di essi: nome e cognome.
+5. Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere all’array creato in precedenza, un nuovo oggetto studente inserendo nell’ ordine: nome, cognome e età.
+BONUS:
+Usiamo il DOM per stampare e chiedere le informazioni all'utente!
+*/
